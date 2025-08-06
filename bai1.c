a64fafe6662cfa24f307402fb2d3e216331e5eeb
Nam Ho <duongvannam286@gmail.com>
2025-08-07 00:09:45 +0700

    fixbug

--- bai1.c
+++ bai1.c
@@ -4,7 +4,7 @@ int main()
 
     printf("helooo nammm");
 
-    printf("da them o day: nam oke lalalalalal toi la dong nghiep 1");
+    printf("da them o day: nam oke lalalalalal toi la dong nghiep 1 toi lai vua sua");
 
     return 0;
 }
