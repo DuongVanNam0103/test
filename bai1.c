2f54b8b62b8e4885555256813a5faaf6cb0b4dd5
Nam Ho <duongvannam286@gmail.com>
2025-08-07 00:17:35 +0700

    ok

--- bai1.c
+++ bai1.c
@@ -2,7 +2,7 @@
 int main()
 {
 
-    printf("helooo nammm toi la robot\n");
+    printf("helooo nammm toi la robot moi them ne nam\n");
 
     printf("da them o day: nam oke lalalalalal toi la dong nghiep 1");
 
